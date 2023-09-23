4813febb736a58cc8485dc898e6d2229009a9bed
leiber <leiberbertel777@gmail.com>
2023-09-22 21:04:13 -0500

    initial-commit

---
+++ src/main/java/com/leiber/market/LeiberMarketApplication.java
@@ -0,0 +1,13 @@
+package com.leiber.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeiberMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeiberMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leiber/market/LeiberMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leiber.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeiberMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
