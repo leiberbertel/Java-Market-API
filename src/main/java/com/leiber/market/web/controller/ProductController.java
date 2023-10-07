14d09106d30f857b45a35308f7bce8c994e4b11f
leiber <leiberbertel777@gmail.com>
2023-10-07 13:17:40 -0500

    feat: add controller product

--- src/main/java/com/leiber/market/web/controller/ProductController.java
+++ src/main/java/com/leiber/market/web/controller/ProductController.java
@@ -2,67 +2,81 @@ package com.leiber.market.web.controller;
 
 import com.leiber.market.domain.Product;
 import com.leiber.market.domain.service.ProductService;
-import com.leiber.market.errors.ResourceNotFoundException;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
-import java.util.Optional;
+import java.util.function.Predicate;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
+
     private final ProductService productService;
 
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
 
-    public List<Product> getAll() {
-        return productService.getAll();
+    @GetMapping("/all")
+    public ResponseEntity<List<Product>> getAll() {
+        return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
-    public Optional<Product> getProduct(int productId) {
-        return productService.getProduct(productId);
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable("id") int productId) {
+        return productService.getProduct(productId)
+                .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    public Optional<List<Product>> getByCategory(int categoryId) {
-        return productService.getByCategory(categoryId);
+    @GetMapping("/category/{categoryId}")
+    public ResponseEntity<List<Product>> getByCategory(@PathVariable("categoryId") int categoryId) {
+        return productService.getByCategory(categoryId).filter(Predicate.not(List::isEmpty)) // filtro únicamente sí la lista no es vacía)
+                .map(products -> new ResponseEntity<>(products, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    public Optional<List<Product>> getScarseProducts(int quantity) {
-        return productService.getScarseProducts(quantity);
+    @GetMapping("/products-scarse/{quantity}")
+    public ResponseEntity<List<Product>> getScarseProducts(@PathVariable("quantity") int quantity) {
+        return productService.getScarseProducts(quantity).filter(Predicate.not(List::isEmpty))
+                .map(products -> new ResponseEntity<>(products, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    public Product save(Product product) {
-        return productService.save(product);
+    @GetMapping("/products-expensive/{price}")
+    public ResponseEntity<List<Product>> getAllProductsExpensive(@PathVariable("price") BigDecimal price) {
+        return productService.getProductExpensive(price).filter(Predicate.not(List::isEmpty))
+                .map(products -> new ResponseEntity<>(products, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    public boolean delete(int productId) {
-        try {
-            productService.delete(productId);
-            return true;
-        } catch (EmptyResultDataAccessException e) {
-            return false;
+    @GetMapping("/products-unavailable/")
+    public ResponseEntity<List<Product>> getAllProductNotAvailable() {
+        return productService.getProductAvailable().filter(Predicate.not(List::isEmpty))
+                .map(products -> new ResponseEntity<>(products, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping()
+    public ResponseEntity<Product> create(@RequestBody Product product) {
+        return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity remove(@PathVariable("id") int productId) {
+        if(productService.delete(productId)) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
-    public Product updateProduct(int productId, Product productoActualizado) {
-        try {
-            Product product = productService.getProduct(productId).orElseThrow(() -> {
-                return new ResourceNotFoundException("Producto", "id", productId);
-            });
-
-            product.setName(productoActualizado.getName());
-            product.setPrice(productoActualizado.getPrice());
-
-            return productService.save(product);
-        } catch (ResourceNotFoundException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new RuntimeException("Error inesperado al actualizar el producto", e);
-        }
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Product> update(@PathVariable("id") int productId, @RequestBody Product productUpdate) {
+        return new ResponseEntity<>(productService.updateProduct(productId, productUpdate), HttpStatus.OK);
     }
-
 }
