288ff0880f39346ac7e525fcbd4e3d69bcab2561
leiber <leiberbertel777@gmail.com>
2023-10-08 15:27:47 -0500

    docs: change API documentation

--- src/main/java/com/leiber/market/web/config/SwaggerConfig.java
+++ src/main/java/com/leiber/market/web/config/SwaggerConfig.java
@@ -1,17 +1,15 @@
 package com.leiber.market.web.config;
 
-import io.swagger.models.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-@EnableWebMvc
 public class SwaggerConfig {
 
     @Bean
@@ -19,6 +17,16 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.leiber.market.web.controller"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API leiberMarket")
+                .description("Esta API ofrece servicios CRUD para un supermercado")
+                .version("1.0")
+                .contact(new Contact("Leiber Bertel", "https://leiberbertel.github.io/", "leiber@gmail.com"))
                 .build();
     }
 }
