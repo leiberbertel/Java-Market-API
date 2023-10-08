574a12214f7477253ed9a8e46c07f0baab764a3c
leiber <leiberbertel777@gmail.com>
2023-10-08 13:17:42 -0500

    docs: add controller documentation

--- src/main/java/com/leiber/market/web/controller/ProductController.java
+++ src/main/java/com/leiber/market/web/controller/ProductController.java
@@ -2,6 +2,10 @@ package com.leiber.market.web.controller;
 
 import com.leiber.market.domain.Product;
 import com.leiber.market.domain.service.ProductService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,39 +26,70 @@ public class ProductController {
     }
 
     @GetMapping("/all")
+    @ApiOperation("Get all supermarket products")
+    @ApiResponse(code = 200, message = "0K")
     public ResponseEntity<List<Product>> getAll() {
         return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Product> getProduct(@PathVariable("id") int productId) {
+    @ApiOperation("Search a product with an id")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "0K"),
+            @ApiResponse(code = 404, message = "Product not found")
+    })
+    public ResponseEntity<Product> getProduct(
+            @ApiParam(value = "The id of the product",
+                    required = true, example = "4")
+            @PathVariable("id") int productId) {
         return productService.getProduct(productId)
                 .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @GetMapping("/category/{categoryId}")
-    public ResponseEntity<List<Product>> getByCategory(@PathVariable("categoryId") int categoryId) {
-        return productService.getByCategory(categoryId).filter(Predicate.not(List::isEmpty)) // filtro únicamente sí la lista no es vacía)
+    @ApiOperation("Search a product with an category id")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "That category does not exist")
+    })
+    public ResponseEntity<List<Product>> getByCategory(@ApiParam(value = "The category id the product", required = true,
+            example = "5") @PathVariable("categoryId") int categoryId) {
+        return productService.getByCategory(categoryId).filter(Predicate.not(List::isEmpty))
                 .map(products -> new ResponseEntity<>(products, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @GetMapping("/products-scarse/{quantity}")
-    public ResponseEntity<List<Product>> getScarseProducts(@PathVariable("quantity") int quantity) {
+    @ApiOperation("Search for all scarce products with an quantity")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "quantity not found")
+    })
+    public ResponseEntity<List<Product>> getScarseProducts(@ApiParam(value = "The quantity", required = true, example = "100") @PathVariable("quantity") int quantity) {
         return productService.getScarseProducts(quantity).filter(Predicate.not(List::isEmpty))
                 .map(products -> new ResponseEntity<>(products, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @GetMapping("/products-expensive/{price}")
-    public ResponseEntity<List<Product>> getAllProductsExpensive(@PathVariable("price") BigDecimal price) {
+    @ApiOperation("Search for all expensive products with an price")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "price not found")
+    })
+    public ResponseEntity<List<Product>> getAllProductsExpensive(@ApiParam(value = "The price", required = true, example = "200.00") @PathVariable("price") BigDecimal price) {
         return productService.getProductExpensive(price).filter(Predicate.not(List::isEmpty))
                 .map(products -> new ResponseEntity<>(products, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @GetMapping("/products-unavailable/")
+    @ApiOperation("Search for all unavailable products")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "All products are available")
+    })
     public ResponseEntity<List<Product>> getAllProductNotAvailable() {
         return productService.getProductAvailable().filter(Predicate.not(List::isEmpty))
                 .map(products -> new ResponseEntity<>(products, HttpStatus.OK))
@@ -62,13 +97,20 @@ public class ProductController {
     }
 
     @PostMapping()
+    @ApiOperation("Create a new Product")
+    @ApiResponse(code = 200, message = "OK")
     public ResponseEntity<Product> create(@RequestBody Product product) {
         return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity remove(@PathVariable("id") int productId) {
-        if(productService.delete(productId)) {
+    @ApiOperation("Delete a product with an id")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "Product not found")
+    })
+    public ResponseEntity remove(@ApiParam(value = "The product id", required = true, example = "10") @PathVariable("id") int productId) {
+        if (productService.delete(productId)) {
             return new ResponseEntity<>(HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -76,7 +118,12 @@ public class ProductController {
     }
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<Product> update(@PathVariable("id") int productId, @RequestBody Product productUpdate) {
+    @ApiOperation("Update a product with an id")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "Product not found")
+    })
+    public ResponseEntity<Product> update(@ApiParam(value = "The product id", required = true, example = "19") @PathVariable("id") int productId, @RequestBody Product productUpdate) {
         return new ResponseEntity<>(productService.updateProduct(productId, productUpdate), HttpStatus.OK);
     }
 }
--- src/main/java/com/leiber/market/web/controller/PurchaseController.java
+++ src/main/java/com/leiber/market/web/controller/PurchaseController.java
@@ -2,6 +2,10 @@ package com.leiber.market.web.controller;
 
 import com.leiber.market.domain.Purchase;
 import com.leiber.market.domain.service.PurchaseService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,18 +24,27 @@ public class PurchaseController {
     }
 
     @GetMapping("/all")
+    @ApiOperation("Get all purchases")
+    @ApiResponse(code = 200, message = "OK")
     public ResponseEntity<List<Purchase>> all() {
         return new ResponseEntity<>(purchaseService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{clientId}")
-    public ResponseEntity<List<Purchase>> getPurchaseByClient(@PathVariable("clientId") String clientId) {
+    @ApiOperation("Search a purchase with a client id")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "Purchase not found")
+    })
+    public ResponseEntity<List<Purchase>> getPurchaseByClient(@ApiParam(value = "The purchase id", required = true, example = "4546221") @PathVariable("clientId") String clientId) {
         return purchaseService.getClientById(clientId).filter(Predicate.not(List::isEmpty))
                 .map(purchases -> new ResponseEntity<>(purchases, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping()
+    @ApiOperation("Create a new purchase")
+    @ApiResponse(code = 200, message = "OK")
     public ResponseEntity<Purchase> save(@RequestBody Purchase purchase) {
         return new ResponseEntity<>(purchaseService.save(purchase), HttpStatus.OK);
     }
