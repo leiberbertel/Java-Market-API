85bcbe254f834892ee28c1100f51ada40da25d61
leiber <leiberbertel777@gmail.com>
2023-10-08 12:13:11 -0500

    docs: add file to document with Swagger

---
+++ src/main/java/com/leiber/market/web/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.leiber.market.web.config;
+
+import io.swagger.models.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.leiber.market.web.controller"))
+                .build();
+    }
+}
