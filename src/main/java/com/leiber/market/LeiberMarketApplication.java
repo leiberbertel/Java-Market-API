d5f33ad7e6bfbff5b7764bc8de1b877ee2d1e133
leiber <leiberbertel777@gmail.com>
2023-10-08 15:26:07 -0500

    style: remove comments

--- src/main/java/com/leiber/market/LeiberMarketApplication.java
+++ src/main/java/com/leiber/market/LeiberMarketApplication.java
@@ -3,7 +3,7 @@ package com.leiber.market;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication // Le dice a Spring que está es la clase que debe correr
+@SpringBootApplication
 public class LeiberMarketApplication {
 
 	public static void main(String[] args) {
