3c457351f9a477e93cab7baf1c196bfbd14627d0
leiber <leiberbertel777@gmail.com>
2023-10-08 10:45:06 -0500

    feat: add purchase controller

--- src/main/java/com/leiber/market/web/controller/PurchaseController.java
+++ src/main/java/com/leiber/market/web/controller/PurchaseController.java
@@ -1,2 +1,38 @@
-package com.leiber.market.web.controller;public class PurchaseController {
+package com.leiber.market.web.controller;
+
+import com.leiber.market.domain.Purchase;
+import com.leiber.market.domain.service.PurchaseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@RestController
+@RequestMapping("/purchases")
+public class PurchaseController {
+
+    private final PurchaseService purchaseService;
+
+    public PurchaseController(PurchaseService purchaseService) {
+        this.purchaseService = purchaseService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Purchase>> all() {
+        return new ResponseEntity<>(purchaseService.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{clientId}")
+    public ResponseEntity<List<Purchase>> getPurchaseByClient(@PathVariable("clientId") String clientId) {
+        return purchaseService.getClientById(clientId).filter(Predicate.not(List::isEmpty))
+                .map(purchases -> new ResponseEntity<>(purchases, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping()
+    public ResponseEntity<Purchase> save(@RequestBody Purchase purchase) {
+        return new ResponseEntity<>(purchaseService.save(purchase), HttpStatus.OK);
+    }
 }
