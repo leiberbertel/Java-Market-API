13a3f95fe599b8a7f9ac36efd6ed1c57d6320f1d
leiber <leiberbertel777@gmail.com>
2023-10-08 10:40:37 -0500

    refactor: change in the findByIdCliente method so that it receives a string

--- src/main/java/com/leiber/market/persistence/crud/CompraCrudRepository.java
+++ src/main/java/com/leiber/market/persistence/crud/CompraCrudRepository.java
@@ -3,9 +3,10 @@ package com.leiber.market.persistence.crud;
 import com.leiber.market.persistence.entity.Compra;
 import org.springframework.data.repository.CrudRepository;
 
+import java.security.Security;
 import java.util.List;
 import java.util.Optional;
 
 public interface CompraCrudRepository extends CrudRepository<Compra, Integer> {
-    Optional<List<Compra>> findByIdCliente(Integer idCliente);
+    Optional<List<Compra>> findByIdCliente(String idCliente);
 }
